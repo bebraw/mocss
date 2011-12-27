37d5e854ea511f47e31e09d9de724ab3cd79ed06
Juho Vepsalainen <bebraw@gmail.com>
2011-12-27 17:18:39 +0200

    Implement initial syntax tree generator

--- src/more.js
+++ src/more.js
@@ -15,6 +15,44 @@ fs.readFile(source, 'utf-8', function(err, data) {
         });
     };
 
+    var treeify = function(input) {
+        var parents = [];
+
+        return input.filter(function(elem) {return elem.l || elem.r;}).
+                map(function(line) {
+            parent = parents[parents.length - 1] || {ind: 0};
+
+            elem = {name: line.r, children: [], ind: line.l.length};
+
+            if (parent.ind == elem.ind) {
+                parents.pop();
+                parent = parents[parents.length -1] || {ind: 0};
+            }
+
+            if (elem.ind == 0) {
+                parents = [elem];
+                return elem;
+            }
+            else if (parent.ind < elem.ind) {
+                parents.push(elem);
+                parent.children.push(elem);
+            }
+        }).filter(function(elem) {return elem;});
+    }
+
+    var printTree = function(tree, i) {
+        i = i || 0;
+
+        tree.forEach(function(k) {
+            if (k.children.length > 0) {
+                console.log('depth: ' + i);
+                console.log(k);
+                
+                printTree(k.children, i + 1);
+            }
+        });
+    }
+
     var transform = function(lines) {
         var bracketFound = false;
 
@@ -35,7 +73,10 @@ fs.readFile(source, 'utf-8', function(err, data) {
         });
     };
 
-    var output = transform(analyze(data.split('\n'))).join('\n');
+    var parts = analyze(data.split('\n'));
+    var tree = treeify(parts);
+    printTree(tree);
+    var output = transform(tree).join('\n');
 
     fs.writeFile(target, output, function(err) {
         if (err) throw err;
