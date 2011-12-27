5f7fd486de4d67e664b79d9b270cc4c2f03f5a99
Juho Vepsalainen <bebraw@gmail.com>
2011-12-27 15:21:05 +0200

    Make it possible to transform indented code to valid CSS

--- src/more.js
+++ src/more.js
@@ -7,7 +7,37 @@ var target = source.substring(0, source.lastIndexOf('.')) + '.css';
 fs.readFile(source, 'utf-8', function(err, data) {
     if (err) throw err;
 
-    fs.writeFile(target, data, function(err) {
+    var analyze = function(input) {
+        return input.map(function(line) {
+            var rPart = line.trimLeft();
+
+            return {l: line.replace(rPart, ''), r: rPart};
+        });
+    };
+
+    var transform = function(lines) {
+        var bracketFound = false;
+
+        return lines.map(function(line) {
+            if (line.l) {
+                return line.l + line.r + ';';
+            }
+            else if (line.r) {
+                bracketFound = true;
+                
+                return line.r + ' {';
+            }
+            else if (bracketFound) {
+                bracketFound = false;
+
+                return '}';
+            }
+        });
+    };
+
+    var output = transform(analyze(data.split('\n'))).join('\n');
+
+    fs.writeFile(target, output, function(err) {
         if (err) throw err;
 
         console.log('Wrote ' + target + '!');
